db41d5b43a8319866311c30da13ebc1c1d44fa40
al1103 <phamtuan72az@gmail.com>
2024-08-24 21:40:32 +0700

    fix load image

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 const img = document.getElementById("img");
-document.getElementById("changeImg").addEventListener("change", function() {
-    img.src = `./image/${this.value}.png`;
-})
+const changeImg = document.getElementById("changeImg");
+const imageSizes = ["0", "144p", "240p", "360p", "720p", "1080p"];
+
+// Preload all images
+imageSizes.forEach((size) => {
+  const link = document.createElement("link");
+  link.rel = "prefetch";
+  link.as = "image";
+  link.href = `./image/${size}.png`;
+  document.head.appendChild(link);
+});
+
+// Ensure image 0 is loaded immediately
+img.src = "./image/0.png";
+
+changeImg.addEventListener("change", function () {
+  const selectedSize = this.value;
+  img.src = `./image/${selectedSize}.png`;
+});
